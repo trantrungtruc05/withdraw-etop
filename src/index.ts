97ebd16389f35f9141e60d7969b3820b53c1eb34
Truc Tran <trantrungtruc220691@gmail.com>
2022-04-19 10:54:37 +0700

    Update withdraw dota

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,26 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import * as controller from './controller';
 import connection from './db/connection';
+import * as withdrawService from './service/withdrawService';
+var cron = require('node-cron');
 
 const app: Express = express();
 const port = 3001;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route("/withdraw").get(controller.withdraw);
+app.route("/withdraw/:category").get(controller.withdraw);
 
-app.listen(port, async() => {
+cron.schedule('*/2 * * * * *', async () => {
+  withdrawService.withdraw('csgo');
+});
+
+cron.schedule('*/2 * * * * *', async () => {
+  withdrawService.withdraw('dota');
+});
+
+app.listen(port, async () => {
   await connection.sync();
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
