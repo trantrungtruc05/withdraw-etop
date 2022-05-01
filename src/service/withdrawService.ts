a2b44afecbaf9478a513215510e0ce65a783ecf3
Truc Tran <trantrungtruc220691@gmail.com>
2022-05-01 11:11:05 +0700

    Disable time withdraw

--- src/service/withdrawService.ts
+++ src/service/withdrawService.ts
@@ -8,17 +8,18 @@ var querystring = require('querystring');
 
 export let withdraw = async (category) => {
 
-    let currDate = new Date();
-    console.log(`Server time today is: ${currDate}`);
-    if (currDate.getHours() >= 0 && currDate.getHours() <= 4) {
+    // let currDate = new Date();
+    // console.log(`Server time today is: ${currDate}`);
+    // if (currDate.getHours() >= 0 && currDate.getHours() <= 4) {
+    if (true) {
         console.log(`Connect withdraw etop with ${category}`);
         var proxyLisy = ['45.131.212.199:6248', '45.131.212.96:6145', '45.131.212.239:6288', '45.131.212.154:6203', '45.131.212.54:6103', '45.131.212.147:6196', '45.131.212.134:6183', '45.131.212.230:6279', '45.131.212.8:6057', '45.131.212.223:6272'
             , '45.131.212.243:6292', '45.131.212.110:6159', '45.131.212.139:6188', '45.131.212.116:6165', '45.131.212.164:6213', '45.131.212.196:6245', '45.131.212.250:6299', '45.131.212.228:6277', '45.131.212.240:6289', '45.131.212.211:6260']
 
         var proxy = proxyLisy[Math.floor(Math.random() * proxyLisy.length)];
 
-        var cookieEtopWithdrawItem : ConfigInfo[];
-    
+        var cookieEtopWithdrawItem: ConfigInfo[];
+
         var items;
         if (category === 'csgo') {
             items = await connection.query("select eip.name, eip.id_item idItem, eip.quantity as quantity from queue_etop_item_with_draw qd inner join etopfun_item_page eip on qd.name = eip.name where qd.status = false order by qd.price_by_vnd desc", { type: QueryTypes.SELECT });
@@ -67,7 +68,7 @@ export let withdraw = async (category) => {
             transporter.sendMail(mailOptions);
 
         }
-    }else{
+    } else {
         console.log('No action withdraw');
     }
 };
